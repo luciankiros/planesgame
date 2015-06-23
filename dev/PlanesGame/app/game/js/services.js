bcd9673334d049162f5d78995e6925eec0c26cff
Chirosca Lucian <lucian.kiros@gmail.com>
2015-06-23 09:33:04 +0300

    plnae game - last version

--- dev/PlanesGame/app/game/js/services.js
+++
@@ -1,8 +0,0 @@
-﻿app.factory('utils', function () {
-    return {
-        getBoardCoords: function(x, y) {
-            var letter = String.fromCharCode('A'.charCodeAt(0) + y);
-            return (x + 1) + '-' + letter;
-        }
-    };
-});
